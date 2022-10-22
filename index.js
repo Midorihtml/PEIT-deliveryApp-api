c9e7525b8117e42372b2494c3b4f1f7aea406414
Guillermo Fernandez <uhgl33@gmail.com>
2022-10-22 20:18:37 -0300

    resolve cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
-server.use(cors({ origin: 'http://localhost:3000' }));
+server.use(cors({ origin: ['http://localhost:3000', "https://peit-react1-delivery-app.vercel.app"] }));
 server.use(router);
 
 server.listen(port)
